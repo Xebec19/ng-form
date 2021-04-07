e51ef18f0f71244338af103281ee60a89f9e4c15
xebec19 <thakur190420@gmail.com>
2021-04-07 19:29:57 +0530

    save

--- src/app/new-form/new-form.component.ts
+++ src/app/new-form/new-form.component.ts
@@ -7,16 +7,43 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./new-form.component.css']
 })
 export class NewFormComponent {
-
+  submitted:boolean = false;
+  genders = ['male','female'];
+  answer:string;
   @ViewChild('f') signupForm: NgForm;
 
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: ''
+  };
   suggestUserName(){
     const suggestedName = 'Superuser';
+   /* this.signupForm.setValue({
+      userData:{
+        username: suggestedName,
+        email:''
+      },
+      secret:'pet',
+      gender:'male',
+      answer:'dodido'
+    })*/
+    
+    this.signupForm.form.patchValue({
+      userData:{
+        username: suggestedName
+      }
+    })
   }
   defaultQuestion = "teacher";
 
   onSubmit(f:NgForm){
     console.log('###The form values are ### ',this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.username = this.signupForm.value.username;
   }
   constructor() { }
 
