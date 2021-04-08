f7d5c4c902ae3f2edbb2eb0e88580325b98bbfae
xebec19 <thakur190420@gmail.com>
2021-04-08 09:24:36 +0530

    set up reactice forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import {ReactiveFormsModule} from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NewFormComponent } from './new-form/new-form.component';
@@ -11,7 +11,7 @@ import { NewFormComponent } from './new-form/new-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/new-form/new-form.component.ts
+++ src/app/new-form/new-form.component.ts
@@ -1,51 +1,16 @@
-import { Component,ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { Component } from '@angular/core';
+import {FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-new-form',
   templateUrl: './new-form.component.html',
   styleUrls: ['./new-form.component.css']
 })
 export class NewFormComponent {
-  submitted:boolean = false;
-  genders = ['male','female'];
-  answer:string;
-  @ViewChild('f') signupForm: NgForm;
-
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: ''
-  };
-  suggestUserName(){
-    const suggestedName = 'Superuser';
-   /* this.signupForm.setValue({
-      userData:{
-        username: suggestedName,
-        email:''
-      },
-      secret:'pet',
-      gender:'male',
-      answer:'dodido'
-    })*/
-    
-    this.signupForm.form.patchValue({
-      userData:{
-        username: suggestedName
-      }
-    })
-  }
-  defaultQuestion = "teacher";
-
-  onSubmit(f:NgForm){
-    console.log('###The form values are ### ',this.signupForm);
-    this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.username = this.signupForm.value.username;
-    this.signupForm.reset();
-  }
+  genders=[
+    "male",
+    "female"
+  ]
+  myform: FormGroup;
   constructor() { }
 
 }
