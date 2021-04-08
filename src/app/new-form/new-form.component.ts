c75a55da8e33e5e13837df545ee506ecb4765101
xebec19 <thakur190420@gmail.com>
2021-04-08 10:16:25 +0530

    set up validators

--- src/app/new-form/new-form.component.ts
+++ src/app/new-form/new-form.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import { Component,OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-new-form',
   templateUrl: './new-form.component.html',
   styleUrls: ['./new-form.component.css']
 })
-export class NewFormComponent {
-  genders=[
+export class NewFormComponent implements OnInit {
+  genders = [
     "male",
     "female"
   ]
   myform: FormGroup;
-  constructor() { }
-
+  OnSubmit() {
+    console.log(this.myform.value);
+  }
+  ngOnInit(): void {
+    this.myform = new FormGroup({
+      'username': new FormControl(null,  Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'gender': new FormControl('male')
+    });
+  }
 }
